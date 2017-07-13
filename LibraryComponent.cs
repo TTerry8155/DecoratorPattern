ab127a1859efef6397cee7490670675c269695b9
TTerry8155 <tterry8155@gmail.com>
2017-07-13 16:16:18 -0500

    Added LibraryComponent and Book

---
+++ LibraryComponent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DecoratorPattern
+{
+    public abstract class LibraryComponent
+    {
+        private int _numCopies;
+
+        // Property
+        public int NumCopies
+        {
+            get { return _numCopies; }
+            set { _numCopies = value; }
+        }
+
+        public abstract void Display();
+    }
+}
